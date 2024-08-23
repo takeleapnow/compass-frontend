3ca059a037e5bd6aa30a35b6d095692afc1ae648
Chirag Rajput <103127634+Chirag2203@users.noreply.github.com>
2024-08-23 15:50:39 +0530

    application cards added

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,15 +1,16 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium  transition-colors focus-visible:outline-none gap-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-lightPrimary dark:bg-darkSecondary dark:text-lightAccent text-primary-foreground hover:bg-lightPrimary/90 dark:hover:bg-darkSecondary/90",
+        default:
+          "bg-lightPrimary dark:bg-darkSecondary dark:text-lightAccent text-primary-foreground hover:bg-lightPrimary/90 dark:hover:bg-darkSecondary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -20,12 +21,15 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
         profileBtn:
           "bg-white text-lightPrimary rounded-none border-none hover:bg-lightAccent w-full",
+        sleekTransparent:
+          "bg-transparent text-lightPrimary  border border-lightAccent hover:bg-lightAccent font-normal gap-1 ",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        sleek: "h-8 px-3 py-1",
       },
     },
     defaultVariants: {
@@ -33,26 +37,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
